df1a00f8404d4771e08df0f3f423a73ec560122f
Gaya3 <gayathrirameshb18@gmail.com>
2024-01-07 23:04:25 +0530

    Added Forms

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0400503ac7823818f689b2f033f19bd1'
 posts=[
     {
         'author':'Gayathri Ramesh',
@@ -37,6 +39,16 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form =LoginForm()
+    return render_template('login.html', title='Login',form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
